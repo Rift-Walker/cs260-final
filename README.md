358d8a3965409e3938be994f95b945a168e19aa7
Nathan Walker <nathan.b.walker@vanderbilt.edu>
2014-11-25 23:23:47 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+cs260-final
+===========
+
+Final project for AI
+
---
+++ agents/myagent.py
@@ -0,0 +1,25 @@
+# You will implement this class
+# At the minimum, you need to implement the selectNodes function
+# If you override __init__ from the agent superclass, make sure that the interface remains identical as in agent; 
+# otherwise your agent will fail
+
+from agent import Agent
+    
+class MyAgent(Agent):
+
+    def selectNodes(self, network, t):
+        # select a subset of nodes (up to budget) to seed at current time step t
+        # nodes in the network are selected *** BY THEIR INDEX ***
+        selected = []
+
+        # your code goes here
+        nmax = 0
+        for n in network.nodes:
+            if not n.adopted():
+                
+        
+        return selected
+    
+    def display():
+        print "Agent ID ", self.id
+
